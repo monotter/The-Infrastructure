967ac417c07cc1e7f453eae19a43f7e76190fd28
Mert Çelik <mertav23@gmail.com>
2021-03-29 14:01:13 +0300

    express, bodyparser

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const cors = require("cors")
 const bodyparser = require("body-parser")
 const express = require("express")
 const path = require("path")
-const app = require("express")()
+const app = express()
 const mongo = require("mongoose")
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.set("view engine","ejs")
 app.set('views', path.join(__dirname, '/src/Views'));
